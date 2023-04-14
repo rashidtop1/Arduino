5f3bdf5bf21c28f0f04de81c2860e08a298344fd
rashidtop1 <91308885+rashidtop1@users.noreply.github.com>
2023-04-14 11:49:12 +0400

    Update rashid_task2_taskmanager.ino

--- rashid_task2_taskmanager.ino
+++ rashid_task2_taskmanager.ino
@@ -1,34 +1,84 @@
-// Определяем пины светодиодов
 #include <TaskManager.h>
 
-int ledPins[] = {3, 5, 6, 9, 10, 11};
-int delays[] = {50,100,1000,5000,10000,100000};
-int states[] = {LOW,LOW,LOW,LOW,LOW,LOW};
+const int ledPin = LED_BUILTIN;
 
+int ledState1 = LOW;
+int ledState2 = LOW;
+int ledState3 = LOW;
+int ledState4 = LOW;
+int ledState5 = LOW;
+int ledState6 = LOW;
 
-int numLedPins = 6;
-int i = 0;
-void setup() {
-  // Настраиваем пины светодиодов на выход
-  
-  for (i; i < numLedPins; i++) {
-    taskManager.scheduleFixedRate(delays[i], [] {
-         if (states[i] == LOW) {
-             states[i] = HIGH;
-      } else {
-        states[i] = LOW;
-      }  
-
-       digitalWrite(ledPins[i], states[i]);
-	  }, TIME_MICROS);    
-    
+void toggleLed1() {
+  if (ledState1 == LOW) {
+    ledState1 = HIGH;
+  } else {
+    ledState1 = LOW;
   }
-  //for (int i = 0; i < numLedPins; i++) {
- //   pinMode(ledPins[i], OUTPUT);
-  //}
+  digitalWrite(11, ledState1);
+}
+
+void toggleLed2() {
+  if (ledState2 == LOW) {
+    ledState2 = HIGH;
+  } else {
+    ledState2 = LOW;
+  }
+  digitalWrite(10, ledState2);
+}
+
+void toggleLed3() {
+  if (ledState3 == LOW) {
+    ledState3 = HIGH;
+  } else {
+    ledState3 = LOW;
+  }
+  digitalWrite(9, ledState3);
+}
+
+void toggleLed4() {
+  if (ledState4 == LOW) {
+    ledState4 = HIGH;
+  } else {
+    ledState4 = LOW;
+  }
+  digitalWrite(6, ledState4);
+}
+
+void toggleLed5() {
+  if (ledState5 == LOW) {
+    ledState5 = HIGH;
+  } else {
+    ledState5 = LOW;
+  }
+  digitalWrite(5, ledState5);
+}
+
+void toggleLed6() {
+  if (ledState6 == LOW) {
+    ledState6 = HIGH;
+  } else {
+    ledState6 = LOW;
+  }
+  digitalWrite(3, ledState6);
+}
+
+void setup() {
+  pinMode(11, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(3, OUTPUT);
+
+  taskManager.scheduleFixedRate(100000, toggleLed1, TimerUnit::TIME_MICROS);
+  taskManager.scheduleFixedRate(200000, toggleLed2, TimerUnit::TIME_MICROS);
+  taskManager.scheduleFixedRate(400000, toggleLed3, TimerUnit::TIME_MICROS);
+  taskManager.scheduleFixedRate(500000, toggleLed4, TimerUnit::TIME_MICROS);
+  taskManager.scheduleFixedRate(600000, toggleLed5, TimerUnit::TIME_MICROS);
+  taskManager.scheduleFixedRate(700000, toggleLed6, TimerUnit::TIME_MICROS);
 }
 
 void loop() {
-  // Мигаем светодиодами по очереди
-    taskManager.runLoop();
+  taskManager.runLoop();
 }
